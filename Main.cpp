7ebaa3acb6dabd687e95c95b46699ec70911d28f
Mabdullahrabbani <abdullahrabbani0007@gmail.com>
2026-05-10 13:35:25 +0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,34 @@
 #include "Class.h"
 
 int main() {
+    enableAnsiColors();
     string whiteName, blackName;
-
     while (true) {
         showMainMenu();
+        const string GREEN = "\033[38;5;220m";
+        const string RESET = "\033[0m";
+        cout << GREEN;
         char choice;
         cin >> choice;
-
+        cout << RESET;
         if (choice == '2') {
             if (confirmExit()) {
+
                 clearScreen();
-                cout << "\n  Thanks for playing. Goodbye!\n\n";
+
+                const string GREEN = "\033[38;5;220m";
+                const string RESET = "\033[0m";
+
+                string Padding = "                                        ";
+
+                cout << GREEN;
+
+                cout << "\n\n\n\n\n\n\n\n";
+
+                cout << Padding << "              THANK YOU FOR PLAYING                      \n";
+
+                cout << RESET;
+
                 system("pause");
                 return 0;
             }
@@ -24,10 +41,12 @@ int main() {
         }
         else {
             clearScreen();
+
+            const string GREEN = "\033[38;5;220m";
+            cout << GREEN;
             cout << "\n  Invalid option. Please enter 1 or 2.\n\n";
             system("pause");
         }
     }
-
     return 0;
 }

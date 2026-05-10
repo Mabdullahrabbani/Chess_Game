f17ca340d53d6f4b57db9c31430d852032faa64d
Mabdullahrabbani <abdullahrabbani0007@gmail.com>
2026-05-10 19:07:25 +0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,51 +2,47 @@
 
 int main() {
     enableAnsiColors();
+
+    const string G = "\033[38;5;220m", R = "\033[0m";
+    const string P = "                                        ";
     string whiteName, blackName;
+
     while (true) {
         showMainMenu();
-        const string GREEN = "\033[38;5;220m";
-        const string RESET = "\033[0m";
-        cout << GREEN;
-        char choice;
-        cin >> choice;
-        cout << RESET;
-        if (choice == '2') {
-            if (confirmExit()) {
+        cout << G;
+        char choice; cin >> choice;
+        cout << R;
 
-                clearScreen();
-
-                const string GREEN = "\033[38;5;220m";
-                const string RESET = "\033[0m";
-
-                string Padding = "                                        ";
-
-                cout << GREEN;
-
-                cout << "\n\n\n\n\n\n\n\n";
-
-                cout << Padding << "              THANK YOU FOR PLAYING                      \n";
-
-                cout << RESET;
-
-                system("pause");
-                return 0;
-            }
-        }
-        else if (choice == '1') {
+        if (choice == '1') {
             collectPlayerNames(whiteName, blackName);
             Game game;
             game.setPlayers(whiteName, blackName);
             game.run();
         }
+        else if (choice == '2') {
+            if (confirmExit()) {
+                clearScreen();
+                cout << G << "\n\n\n\n\n\n\n";
+                cout << P << "====================================================\n\n";
+                cout << P << "              THANK YOU FOR PLAYING!                \n\n";
+                cout << P << "====================================================\n\n";
+                cout << P << "              Press ENTER to exit..." << R;
+                cin.ignore(); cin.get();
+                return 0;
+            }
+        }
         else {
             clearScreen();
-
-            const string GREEN = "\033[38;5;220m";
-            cout << GREEN;
-            cout << "\n  Invalid option. Please enter 1 or 2.\n\n";
-            system("pause");
+            cout << G << "\n\n\n\n\n\n\n";
+            cout << P << "====================================================\n\n";
+            cout << P << "                   INVALID INPUT                    \n\n";
+            cout << P << "====================================================\n\n";
+            cout << P << "          Please enter 1 or 2.\n\n";
+            cout << P << "====================================================\n\n";
+            cout << P << "           Press ENTER to try again..." << R;
+            cin.ignore(); cin.get();
         }
     }
+
     return 0;
 }
